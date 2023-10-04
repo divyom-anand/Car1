e584c7ce9b87ac8c054052155fc8162f291ff4b6
Divyom <divyom.champ@gmail.com>
2023-10-04 22:09:03 +0530

    first commit

---
+++ Car1/Car1UITests/Car1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Car1UITestsLaunchTests.swift
+//  Car1UITests
+//
+//  Created by Divyom Anand on 04/10/23.
+//
+
+import XCTest
+
+final class Car1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
